aacd9f92a3ae5f3838f942b2c3b401ca3aa7135f
ArturoEsp <bmth_harcore@hotmail.com>
2026-09-25 15:48:29 -0600

    chore: se corrige error de egresos

--- src/app/expenses/presentation/dtos/responses/expenses-list.response.ts
+++ src/app/expenses/presentation/dtos/responses/expenses-list.response.ts
@@ -1,16 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose, Type } from 'class-transformer';
 import { ExpenseResponse } from './expense.response';
 
 export class ExpensesListResponse {
   @ApiProperty({ example: '2026-09' })
+  @Expose()
   period: string;
 
   @ApiProperty({ example: 8 })
+  @Expose()
   count: number;
 
   @ApiProperty({ example: 23190.5 })
+  @Expose()
   totalAmount: number;
 
   @ApiProperty({ type: [ExpenseResponse] })
+  @Expose()
+  @Type(() => ExpenseResponse)
   records: ExpenseResponse[];
 }
